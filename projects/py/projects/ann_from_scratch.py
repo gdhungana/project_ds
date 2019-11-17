ca4c03d454c5478ab09f9c15c27ccdd204280378
gdhungana <gdhungana100@gmail.com>
2019-11-16 21:32:18 -0600

    add the function to set up inputs and output for the nn layers

--- projects/py/projects/ann_from_scratch.py
+++ projects/py/projects/ann_from_scratch.py
@@ -4,8 +4,33 @@ def activation_fn(x):
     f = 1/(1+np.exp(-x)) #-sigmoid fn
     return f
 
-def weighted_biased_act_fn(x,w=0,b=0):
+def weighted_biased_act_fn(x,w=1,b=0):
     f= 1/(1+np.exp(-(x*w+b)))
     return f
 
 
+def looped_nn_calc(n_layers, x, w, b):
+    for l in range(n_layers-1):
+        #Setup the input array which the weights will be multiplied by for each layer
+        #If it's the first layer, the input array will be the x input vector
+        #If it's not the first layer, the input to the next layer will be the 
+        #output of the previous layer
+        if l == 0:
+            node_in = x
+        else:
+            node_in = h
+        #Setup the output array for the nodes in layer l + 1
+        h = np.zeros((w[l].shape[0],))
+        #loop through the rows of the weight array
+        for i in range(w[l].shape[0]):
+            #setup the sum inside the activation function
+            f_sum = 0
+            #loop through the columns of the weight array
+            for j in range(w[l].shape[1]):
+                f_sum += w[l][i][j] * node_in[j]
+            #add the bias
+            f_sum += b[l][i]
+            #finally use the activation function to calculate the
+            #i-th output i.e. h1, h2, h3
+            h[i] = f(f_sum)
+    return h
